3ab95edcd5a98de9df58bb1d85d576961a14c0dc
Hamadene Lotfi <106031558+CallmeVRM@users.noreply.github.com>
2024-12-14 11:06:20 +0100

    Update and rename populate.sh to delete_user.sh

---
+++ filetransfer/adminpc/scripts/delete_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Affiche un message indiquant le succès de l'exécution
+echo "Le script delete_user.sh est exécuté avec succès"
+
+# Optionnel : on peut aussi ajouter un code de retour pour indiquer le succès (0)
+exit 0
+
