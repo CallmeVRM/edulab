5421bc7a3717d2240201d0ec80c703aa2f7f40d1
Hamadene Lotfi <106031558+CallmeVRM@users.noreply.github.com>
2024-12-14 11:05:56 +0100

    Update create_user.sh

--- filetransfer/adminpc/scripts/create_user.sh
+++ filetransfer/adminpc/scripts/create_user.sh
@@ -1,1 +1,8 @@
-Create USER SH
+#!/bin/bash
+
+# Affiche un message indiquant le succès de l'exécution
+echo "Le script create_user.sh est exécuté avec succès"
+
+# Optionnel : on peut aussi ajouter un code de retour pour indiquer le succès (0)
+exit 0
+
