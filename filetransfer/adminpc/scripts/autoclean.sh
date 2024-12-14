b95f099413f9f9e64bda1baf862f627f6fb38d78
Hamadene Lotfi <106031558+CallmeVRM@users.noreply.github.com>
2024-12-14 11:05:37 +0100

    Update autoclean.sh

--- filetransfer/adminpc/scripts/autoclean.sh
+++ filetransfer/adminpc/scripts/autoclean.sh
@@ -1,1 +1,7 @@
-#autoclean AdminPC
+#!/bin/bash
+
+# Affiche un message indiquant le succès de l'exécution
+echo "Le script autoclean.sh est exécuté avec succès"
+
+# Optionnel : on peut aussi ajouter un code de retour pour indiquer le succès (0)
+exit 0
